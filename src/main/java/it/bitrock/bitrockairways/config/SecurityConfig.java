a4c8e3ed70795f9f37e97641d4686950a587ba52
tommaso-giovannelli-97 <tommaso.giovannelli@bitrock.it>
2023-03-15 16:12:13 +0100

    - Spring security implementation first version

---
+++ src/main/java/it/bitrock/bitrockairways/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package it.bitrock.bitrockairways.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        /*http.authorizeHttpRequests().anyRequest().authenticated().and().oauth2Login()
+                .redirectionEndpoint()
+                .baseUri("/success.html")
+                .and()
+                .defaultSuccessUrl("/login/success");
+        return http.build();*/
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/login2").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login()
+                //.and()
+                //.formLogin()
+                .loginPage("/login2")
+                .defaultSuccessUrl("/login/success");
+
+        return http.build();
+    }
+
+
+    /*@Bean
+    public SecurityFilterChain securityConfig(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and().oauth2Login()
+                .redirectionEndpoint().baseUri("/success.html")
+                .and().defaultSuccessUrl("/loginSuccess")
+                .failureHandler(new SimpleUrlAuthenticationFailureHandler("/loginFailure"))
+                .and().build();
+    }*/
+
+}
---
+++ src/main/java/it/bitrock/bitrockairways/controller/LoginController.java
@@ -0,0 +1,56 @@
+package it.bitrock.bitrockairways.controller;
+
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login/success")
+    public String success() {
+        return "success.html";
+    }
+
+    @GetMapping("/login2")
+    public String login() {
+        return "login.html";
+    }
+
+    @GetMapping("/success")
+    public String main(OAuth2AuthenticationToken token) {
+        System.out.println(token.getPrincipal());
+        return "success.html";
+    }
+
+    /*private final Iterable<ClientRegistration> clientRegistrationRepository;
+
+    public LoginController(Iterable<ClientRegistration> clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
+    @GetMapping("/oauth_login")
+    public String login(Model model) {
+        Map<String, String> repositories = new HashMap<>();
+        clientRegistrationRepository.forEach(repository ->
+                repositories.put(
+                        repository.getClientName(),
+                        "oauth2/authorization/" + repository.getRegistrationId()
+                ));
+        model.addAttribute("urls", repositories);
+        return "login";
+    }
+
+    @GetMapping("/loginSuccess")
+    public String loginSuccess(Model model, OAuth2AuthenticationToken token) {
+        OAuth2User principal = token.getPrincipal();
+        model.addAttribute("username", principal.getAttribute("name"));
+        return "login-success";
+    }
+
+    @GetMapping("/loginFailure")
+    public String loginFailure() {
+        return "login-failure";
+    }*/
+}
